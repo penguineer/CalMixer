e8ba3dea7fe74e19f84995bcd8f5630e46d3ac27
Stefan Haun <tux@netz39.de>
2022-06-05 17:01:29 +0200

    Add an UptimeHealthProvider

---
+++ src/main/java/com/penguineering/calmixer/UptimeHealthProvider.java
@@ -0,0 +1,28 @@
+package com.penguineering.calmixer;
+
+import jakarta.inject.Singleton;
+
+import java.time.Duration;
+import java.util.Optional;
+
+@Singleton
+public class UptimeHealthProvider implements HealthProvider {
+    private final long start = System.currentTimeMillis();
+
+    @Override
+    public String getKey() {
+        return "uptime";
+    }
+
+    @Override
+    public Optional<Object> getStatus() {
+        final long now = System.currentTimeMillis();
+        final String duration = Duration.ofMillis(now - this.start).toString();
+        return Optional.of(duration);
+    }
+
+    @Override
+    public boolean isHealthy() {
+        return true;
+    }
+}
