dd1448d2151acb574d9e61cfa04e777828b41750
Stefan Haun <tux@netz39.de>
2022-06-05 17:20:06 +0200

    Add tests for Version and Update health providers

--- src/test/java/com/penguineering/calmixer/health/HealthTest.java
+++ src/test/java/com/penguineering/calmixer/health/HealthTest.java
@@ -5,8 +5,15 @@ import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,5 +33,39 @@ public class HealthTest {
         final JSONObject status = new JSONObject(body);
         assertTrue((Boolean) status.query("/healthy"));
         assertNotNull(status.query("/uptime"));
+    }
+
+    @Inject
+    @Named("Version")
+    HealthProvider versionHealthProvider;
+
+    @Test
+    void testVersionHealthProvider() {
+        // return correct key
+        assertEquals("version", versionHealthProvider.getKey());
+        // in the test context there is no package, so we will not get a version
+        assertTrue(versionHealthProvider.getStatus().isEmpty());
+        // always healthy
+        assertTrue(versionHealthProvider.isHealthy());
+    }
+
+    @Inject
+    @Named("Uptime")
+    HealthProvider uptimeHealthProvider;
+
+    @Test
+    void testUptimeHealthProvider() {
+        // return correct key
+        assertEquals("uptime", uptimeHealthProvider.getKey());
+        // must return a correct ISO8601 duration
+        Optional<Object> duration = uptimeHealthProvider.getStatus();
+        assertTrue(duration.isPresent());
+        try {
+            Duration.parse((String)duration.get());
+        } catch (DateTimeParseException e) {
+            fail("Failed to parse duration from UptimeHealthProvider: " + duration.get());
+        }
+        // always healthy
+        assertTrue(uptimeHealthProvider.isHealthy());
     }
 }
