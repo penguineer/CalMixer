7738a0ea68e0a1bf5dac62225c9782ad13286a2d
Stefan Haun <tux@netz39.de>
2022-06-05 21:55:21 +0200

    Add OAS3 header to application

--- src/main/java/com/penguineering/calmixer/Application.java
+++ src/main/java/com/penguineering/calmixer/Application.java
@@ -1,7 +1,20 @@
 package com.penguineering.calmixer;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Calendar Mixer",
+                version = "0.1",
+                description = "REST API of the Calendar Mixer",
+                license = @License(name = "MIT", url = "https://github.com/penguineer/CalMixer/blob/main/LICENSE.txt"),
+                contact = @Contact(name = "Stefan Haun", email = "tux@netz39.de")
+        )
+)
 public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
