55e83c83bfa263b4b6dad8733a17dfb54a581c43
Stefan Haun <tux@netz39.de>
2022-06-05 16:38:38 +0200

    Add a HealthProvider interface

---
+++ src/main/java/com/penguineering/calmixer/HealthProvider.java
@@ -0,0 +1,26 @@
+package com.penguineering.calmixer;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Provide health informatin for a sub-system.
+ */
+public interface HealthProvider {
+    /**
+     * @return a key for this health provider
+     */
+    String getKey();
+
+    /**
+     * Get health status information
+     * @return Map of health status from this provider
+     */
+    Optional<Object> getStatus();
+
+    /**
+     * State if this provider's status is considered healthy
+     * @return True if healthy, otherwise false
+     */
+    boolean isHealthy();
+}
