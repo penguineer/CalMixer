0bf2fb3cac3d08bf30ddb3b7ee3e0c804d8eb9d5
Stefan Haun <tux@netz39.de>
2022-06-13 18:59:48 +0200

    Extact access information from source parameters

--- src/main/java/com/penguineering/calmixer/calendar/CalSource.java
+++ src/main/java/com/penguineering/calmixer/calendar/CalSource.java
@@ -1,23 +1,38 @@
 package com.penguineering.calmixer.calendar;
 
 import net.jcip.annotations.Immutable;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.Map;
 
 @Immutable
 public class CalSource {
-    public static CalSource withValues(final URL url, final String label) {
-        return new CalSource(url, label);
+    public static CalSource withJson(final JSONObject json) {
+        try {
+            final URL url = URI.create(json.getString("url")).toURL();
+            final String label = json.getString("label");
+
+            CalAccess access = CalAccess.withJson(json);
+
+            return new CalSource(url, label, access);
+        } catch (JSONException | MalformedURLException e) {
+            throw new IllegalArgumentException(e);
+        }
+
     }
 
     private final URL url;
     private final String label;
+    private final CalAccess access;
 
-    private CalSource(final URL url, final String label) {
+    private CalSource(final URL url, final String label, final CalAccess access) {
         this.url = url;
         this.label = label;
+        this.access = access;
     }
 
     public URL getUrl() {
@@ -28,10 +43,15 @@ public class CalSource {
         return label;
     }
 
+    public CalAccess getAccess() {
+        return access;
+    }
+
     public JSONObject toJson() {
         return new JSONObject(Map.of(
                 "url", url,
-                "label", label
+                "label", label,
+                "access", access.toJson()
         ));
     }
 }
